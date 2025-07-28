82c56e93ab540b1b466fe7ba64507ac98a4cdfa9
themusecatcher <themusecatcher@163.com>
2025-07-28 17:50:23 +0800

    feat: update project config

--- config/vite.config.dev.ts
+++ config/vite.config.dev.ts
@@ -1,15 +1,30 @@
-import { mergeConfig } from 'vite';
+import { defineConfig, loadEnv, mergeConfig } from 'vite';
 import baseConfig from './vite.config.base';
 
-export default mergeConfig(
-  {
-    mode: 'development',
-    server: {
-      open: true,
-      fs: {
-        strict: true,
+export default defineConfig(({ mode }) => {
+  // 加载环境变量 (注意路径调整)
+  const env = loadEnv(mode, process.cwd(), '');
+
+  // 获取 API 基础 URL
+  const apiBaseUrl = env.VITE_API_BASE_URL;
+  console.log('apiBaseUrl', apiBaseUrl);
+
+  return mergeConfig(
+    {
+      mode: 'development',
+      server: {
+        host: true,
+        open: true,
+        port: 3000,
+        proxy: {
+          '/api': {
+            target: apiBaseUrl,
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/api/, ''),
+          },
+        },
       },
     },
-  },
-  baseConfig
-);
+    baseConfig
+  );
+});
