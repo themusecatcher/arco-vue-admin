90eb15f9df332219c09a838974c709c25f60d97b
themusecatcher <themusecatcher@163.com>
2025-07-28 17:54:09 +0800

    feat: update prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,8 +2,9 @@ module.exports = {
   tabWidth: 2,
   semi: false,
   printWidth: 120,
+  trailingComma: 'none',
   singleQuote: true,
   quoteProps: 'consistent',
   htmlWhitespaceSensitivity: 'strict',
-  vueIndentScriptAndStyle: true,
+  vueIndentScriptAndStyle: true
 }
