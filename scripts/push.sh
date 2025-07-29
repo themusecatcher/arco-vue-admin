c2e9177573e143548b873a78850ac8fcd39ea280
themusecatcher <themusecatcher@163.com>
2025-07-29 14:37:51 +0800

    feat: update package config

---
+++ scripts/push.sh
@@ -0,0 +1,20 @@
+# /bin/bash
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 提交修改到github
+
+commitDesc=$1
+
+git add .
+
+if [ -z "$commitDesc" ]; then
+  git commit -m 'update'
+else
+  git commit -m "$commitDesc"
+fi
+
+git push
+
+echo ⏰ "$(date '+%Y-%m-%d %H:%M:%S')"
